2c59a1b0b83c7f2b7929b52a1479885c916af8b1
Liam Forde <93612669+liamAthookie@users.noreply.github.com>
2025-09-29 14:55:12 +0200

    Updated ping-message-handler.ts

--- src/websocket/message-handlers/handlers/ping-message-handler.ts
+++ src/websocket/message-handlers/handlers/ping-message-handler.ts
@@ -3,6 +3,7 @@ import { Session } from '../../../common/session';
 import { MessageHandler } from '../message-handler';
 
 export class PingMessageHandler implements MessageHandler {
+    handle(message: ClientMessage, session: Session): void {
         session.send(session.createMessage('pong', {}));
     }
 }
